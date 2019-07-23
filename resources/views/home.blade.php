2689678e8cb8b3e9375b8d95e86f5e627a0fcb01
j2deme <j2deme@gmail.com>
2019-07-23 00:23:47 -0500

    Añade scaffolding básico de interfaz

---
+++ resources/views/home.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="ui middle aligned center aligned grid">
+  <div class="ui row">
+    <div class="column">
+      <h1 id="landing-page-header" class="ui icon header">
+        <i class="large icons">
+          <i class="blue sort up icon"></i>
+          <i class="red sort down icon"></i>
+        </i>
+        <div class="content">
+          MAB
+          <div class="sub header">Módulo de Altas y Bajas</div>
+        </div>
+      </h1>
+    </div>
+  </div>
+  <div class="row">
+    <div class="column">
+      <a href="#" class="ui primary labeled icon button">
+        <i class="sign in icon"></i>
+        Ingresar
+      </a>
+      <a href="#" class="ui secondary labeled icon button">
+        <i class="id card icon"></i>
+        Registro
+      </a>
+    </div>
+  </div>
+</div>
+@endsection
