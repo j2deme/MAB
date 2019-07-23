7b594bb7e9f9729f4ff13ff0c7dc4c586a5e56c6
j2deme <j2deme@gmail.com>
2019-07-23 15:17:41 -0500

    Ajusta barra de navegación

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,34 +1,40 @@
-<div class="ui fixed inverted menu">
+<div class="ui fixed menu">
   <div class="ui container">
     <a href="{{ url('/') }}" class="header item">
-      <i class="logo icons">
+      <i class="icons">
         <i class="blue sort up icon"></i>
         <i class="red sort down icon"></i>
       </i>
       MAB
     </a>
-    @auth
-    <a href="{{ url('/') }}" class="item">
-      <i class="home icon"></i>
-    </a>
-    <div class="ui simple dropdown item">
-      Dropdown <i class="dropdown icon"></i>
-      <div class="menu">
-        <a class="item" href="#">Link Item</a>
-        <a class="item" href="#">Link Item</a>
-        <div class="divider"></div>
-        <div class="header">Header Item</div>
-        <div class="item">
-          <i class="dropdown icon"></i>
-          Sub Menu
-          <div class="menu">
-            <a class="item" href="#">Link Item</a>
-            <a class="item" href="#">Link Item</a>
+    <div class="right menu">
+      @auth
+      <a href="{{ url('/') }}" class="item">
+        <i class="home icon"></i>
+      </a>
+      <div class="ui simple dropdown item">
+        {{ Auth::user()->name }} <i class="dropdown icon"></i>
+        <div class="menu">
+          <a class="item" href="#">Link Item</a>
+          <a class="item" href="#">Link Item</a>
+          <div class="divider"></div>
+          <div class="header">Header Item</div>
+          <div class="item">
+            <i class="dropdown icon"></i>
+            Sub Menu
+            <div class="menu">
+              <a class="item" href="#">Link Item</a>
+              <a class="item" href="#">Link Item</a>
+            </div>
           </div>
+          <a class="item" href="#">Link Item</a>
+          <a href="{{ url('/logout') }}" class="item"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
         </div>
-        <a class="item" href="#">Link Item</a>
       </div>
+      @guest
+      <a href="{{ url('/login') }}" class="item">Iniciar sesión</a>
+      <a href="{{ url('/register') }}" class="item">Registro</a>
+      @endauth
     </div>
-    @endauth
   </div>
 </div>
