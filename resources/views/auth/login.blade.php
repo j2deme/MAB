2a0a1a74c4ac7891ad685f2fb389b49a4292f2c8
j2deme <j2deme@gmail.com>
2019-07-23 15:32:38 -0500

    Añade navbar a vista login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+@include('layouts.navbar')
 <div class="ui main text container">
   <div class="ui middle aligned centered grid">
     <div class="twelve wide mobile eight wide computer column">
@@ -17,31 +18,12 @@
               <i class="at icon"></i>
               <input type="text" name="email" placeholder="Correo electrónico" autocomplete="email" autofocus>
             </div>
-
-    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-      <label for="password" class="col-md-4 control-label">Password</label>
-
-      <div class="col-md-6">
-        <input id="password" type="password" class="form-control" name="password">
-
-        @if ($errors->has('password'))
-        <span class="help-block">
-          <strong>{{ $errors->first('password') }}</strong>
-        </span>
-        @endif
           </div>
           <div class="field">
             <div class="ui left icon input">
               <i class="lock icon"></i>
               <input type="password" name="password" placeholder="Contraseña" autocomplete="current-password">
             </div>
-
-    <div class="form-group">
-      <div class="col-md-6 col-md-offset-4">
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" name="remember"> Remember Me
-          </label>
           </div>
           <div class="inline field">
             <div class="ui checkbox">
@@ -66,7 +48,6 @@
           ¿No tienes cuenta? <a href="{{ url('/register') }}">Regístrate</a>
         </div>
       </div>
-  </form>
     </div>
   </div>
 </div>
