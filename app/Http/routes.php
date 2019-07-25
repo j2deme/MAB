cd25b5188a0c733ecb798719899dc428935322a2
j2deme <j2deme@gmail.com>
2019-07-25 15:21:33 -0500

    Resuelve rutas nombradas @1bc6279

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,23 @@
 |
 */
 
-Route::get('/', 'PublicController@index');
+Route::get('/', 'PublicController@index')->name('root');
 
-Route::auth();
+# Route::auth(); # Removed in order to name auth routes
 
-Route::get('/home', 'HomeController@index');
+# LOG IN/OUT Routes
+Route::get('login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@showLoginForm']);
+Route::post('login', ['as' => 'auth.login', 'uses' => 'Auth\AuthController@login']);
+Route::get('logout', ['as' => 'auth.logout', 'uses' => 'Auth\AuthController@logout']);
+
+# REGISTRATION ROUTES
+Route::get('register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@showRegistrationForm']);
+Route::post('register', ['as' => 'auth.register', 'uses' => 'Auth\AuthController@register']);
+
+# PASSWORD RESET ROUTES
+Route::get('password/reset/{token?}', ['as' => 'auth.password.reset', 'uses' => 'Auth\PasswordController@showResetForm']);
+Route::post('password/email', ['as' => 'auth.password.email', 'uses' => 'Auth\PasswordController@sendResetLinkEmail']);
+Route::post('password/reset', ['as' => 'auth.password.reset', 'uses' => 'Auth\PasswordController@reset']);
+
+# CUSTOM CONTROLLERS
+Route::get('/home', 'HomeController@index')->name('home.index');
