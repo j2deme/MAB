ab6c1a7c3c43abedad6fca19003aa871c2d83d37
j2deme <j2deme@gmail.com>
2019-07-25 13:58:01 -0500

    Añade UX con valor anterior y autocomplete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,31 +16,32 @@
           <div class="fields">
             <div class="field">
               <label>Nombre</label>
-              <input type="text" id="name" name="name" autofocus />
+              <input type="text" id="name" name="name" value="{{ old('name') }}" autofocus />
             </div>
             <div class="field">
               <label for="last_name">Apellidos</label>
-              <input type="text" id="last_name" name="last_name" />
+              <input type="text" id="last_name" name="last_name" value="{{ old('last_name') }}"
+                autocomplete="family-name" />
             </div>
           </div>
           <div class="fields">
             <div class="field">
               <label for="password">Contraseña</label>
-              <input type="password" id="password" name="password">
+              <input type="password" id="password" name="password" autocomplete="new-password">
             </div>
             <div class="field">
               <label for="password_confirm">Confirmación contraseña</label>
-              <input type="password" id="password_confirm" name="password_confirm">
+              <input type="password" id="password_confirm" name="password_confirm" autocomplete="off">
             </div>
           </div>
           <div class="fields">
             <div class="field">
               <label for="username">Número de control</label>
-              <input type="text" id="username" name="username" />
+              <input type="text" id="username" name="username" value="{{ old('username') }}" />
             </div>
             <div class="field">
               <label for="email">Correo electrónico</label>
-              <input type="text" id="email" name="email" />
+              <input type="text" id="email" name="email" value="{{ old('email') }}" autocomplete="email" />
             </div>
           </div>
           <button type="submit" class="ui fluid primary labeled icon submit button">
