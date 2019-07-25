7f214f626aae6cf86c5d8be921ceec5d3fce0cc4
j2deme <j2deme@gmail.com>
2019-07-25 11:47:24 -0500

    Añade restricción de _username_ único

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
       $table->increments('id');
       $table->string('name');
       $table->string('last_name')->nullable();
-      $table->string('username');
+      $table->string('username')->unique();
       $table->string('email')->unique();
       $table->string('password');
       $table->boolean('is_suspended')->default(false);
