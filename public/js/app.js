afdd7b70b30ef2facb331a27743b5c633f8365eb
j2deme <j2deme@gmail.com>
2019-07-26 18:35:19 -0500

    Agrega JS para cierre de mensajes de alerta
    - Permite el cierre de los mensajes de alerta con "x"
    - Agrega función global JS para el cierre de mensajes de alerta
    - Elimina la necesidad de activar la función de cierre para cada alerta

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 $(function () {
   $('.ui.checkbox').checkbox();
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function () {
+    $(this).closest('.message').transition('fade');
+  });
 });
-
