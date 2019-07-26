dbaae95b4334ad55e1a9d77ea03aae9e09ba90a2
j2deme <j2deme@gmail.com>
2019-07-26 11:50:50 -0500

    Actualiza controlador de autenticación de sesión
    - Añade información extra al modelo usuario
    - Actualiza validador y creación de nuevo usuario
    - Actualiza ruta para redirección al iniciar sesión
    - Permite el inicio de sesión vía username

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
+  /*
     |--------------------------------------------------------------------------
     | Registration & Login Controller
     |--------------------------------------------------------------------------
@@ -21,52 +21,70 @@ class AuthController extends Controller
     |
     */
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+  use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    /**
-     * Where to redirect users after login / registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/';
+  /**
+   * Where to redirect users after login / registration.
+   *
+   * @var string
+   */
+  protected $redirectTo = '/home';
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
-    }
+  /**
+   * Which column will be used for username check
+   */
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-        ]);
-    }
+  protected $username = 'username';
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
-    }
+  /**
+   * Create a new authentication controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware($this->guestMiddleware(), ['except' => 'logout']);
+  }
+
+  /**
+   * Get a validator for an incoming registration request.
+   *
+   * @param  array  $data
+   * @return \Illuminate\Contracts\Validation\Validator
+   */
+  protected function validator(array $data)
+  {
+    return Validator::make($data, [
+      'name'      => 'required|max:255',
+      'last_name' => 'required|max:255',
+      'username'  => 'required|max:255|unique:users',
+      'email'     => 'required|email|max:255|unique:users',
+      'password'  => 'required|min:6|confirmed',
+    ], [
+      'name.required'      => 'Ingresa tu nombre o nombres',
+      'last_name.required' => 'Ingresa tus apellidos',
+      'username.required'  => 'Se requiere usuario / no. de control',
+      'password.required'  => 'Ingresa tu contraseña',
+      'password.confirmed' => 'Las contraseñas no coinciden',
+      'email.required'     => 'Ingresa tu correo electrónico',
+      'email.email'        => 'Ingresa un correo electrónico válido',
+    ]);
+  }
+
+  /**
+   * Create a new user instance after a valid registration.
+   *
+   * @param  array  $data
+   * @return User
+   */
+  protected function create(array $data)
+  {
+    return User::create([
+      'name'      => $data['name'],
+      'last_name' => $data['last_name'],
+      'username'  => $data['username'],
+      'email'     => $data['email'],
+      'password'  => bcrypt($data['password']),
+    ]);
+  }
 }
