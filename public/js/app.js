09a83e06fa5279b8dc4f4aa46f8eb11c8510ee3a
j2deme <j2deme@gmail.com>
2019-08-01 20:18:22 -0500

    Añade función global para conteo de caracteres en JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
+(function ($) {
+  'use strict';
+  $.fn.charsCounter = function () {
+    return this.on('focus keyup', function () {
+      let maxLength = $(this).attr('maxlength');
+      let currentLength = $(this).val().length;
+      let chars = maxLength - currentLength;
+
+      if (chars >= 0 && chars <= 10) {
+        $(this).next('.chars').text(chars).addClass('red text');
+      } else {
+        $(this).next('.chars').text(chars).removeClass('red text');
+      }
+
+      $(this).next('.chars').text(chars + "/" + maxLength);
+    });
+  };
+})(jQuery);
+
 $(function () {
   $('.ui.checkbox').checkbox();
   $('.message .close').on('click', function () {
