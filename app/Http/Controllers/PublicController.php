444243719e4b3e481bb9b895897daa596596579a
j2deme <j2deme@gmail.com>
2019-08-17 12:55:17 -0500

    Añade redirección con sesión iniciada

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use Auth;
 use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
   public function index()
   {
+    if (!is_null(Auth::user())) {
+      return redirect()->route('home.index');
+    }
     return view('index');
   }
 }
