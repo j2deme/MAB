71f3e97287b63eb13ac6e260d9ae4a209013083b
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2023-08-16 16:08:50 -0600

    💄 Agrega vista móvil para movimientos

--- resources/views/moves/index.blade.php
+++ resources/views/moves/index.blade.php
@@ -39,96 +39,149 @@ Path: {{ Request::path() }}
         @endif
         @endhasanyrole
         </div>
-        <table class="ui celled striped compact table">
-          <thead>
-            <tr>
-              <th class="ui center aligned two wide">No. Control</th>
-              <th class="ui center aligned">Solicitud</th>
-              <th class="ui center aligned two wide">Tipo</th>
-              <th class="ui center aligned one wide">Paralelo</th>
-              <th class="ui center aligned two wide">Estatus</th>
-              @can('edit_moves')
-              <th class="ui center aligned two wide">
-                <i class="ui cog icon"></i>
-              </th>
-              @endcan
-            </tr>
-          </thead>
-          <tbody>
-            @forelse ($result as $item)
-            <tr>
-              <td class="ui center aligned">{{ $item->user->username }}</td>
-              <td>{{ $item->type }} DE {{ $item->group->subject->short_name }} ({{ $item->group->full_key }})</td>
-              <td class="ui center aligned">
-                <small>{{ $item->type }}</small> <i class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i>
-              </td>
-              <td class="ui center aligned">
-                @if ($item->is_parallel)
-                <a class="ui blue circular label">P</a>
-                @else
-                <i class="ui grey minus icon"></i>
-                @endif
-              </td>
-              <td class="ui center aligned">
-                @include('shared._move_status', ['status' => $item->status])
-              </td>
-              @can('edit_moves')
-              <td class="ui center aligned">
-                @role('Estudiante')
-                @include('moves.student_actions', ['id' => $item->id, 'move' => $item])
-                @endrole
-                @hasanyrole(['Coordinador','Jefe','Admin'])
-                @include('moves.coordinator_actions', ['id' => $item->id])
-                @endhasanyrole
-              </td>
-              @endcan
-            </tr>
-            @empty
-            <tr>
-              <td colspan="6">
-                <div class="ui placeholder segment">
-                  <div class="ui icon header">
-                    <i class="inbox icon"></i>
-                    @role('Estudiante')
-                    Aún no has solicitado movimientos
-                    @else
-                    No hay movimientos solicitados
-                    @endrole
-                  </div>
-                  @if (!is_null(Auth::user()->career))
-                  @role('Estudiante')
-                  <div class="inline">
-                    <a href="{{ route('moves.new',['type' => 'up']) }}" class="ui blue icon labeled button">
-                      <i class="ui sort up icon"></i>
-                      Altas
-                    </a>
-                    <a href="{{ route('moves.new',['type' => 'down']) }}" class="ui red icon labeled button">
-                      <i class="ui sort down icon"></i>
-                      Bajas
-                    </a>
-                  </div>
-                  @endrole
+
+        <div class="tablet only computer only large screen only widescreen only sixteen wide column">
+          <table class="ui celled striped compact table">
+            <thead>
+              <tr>
+                <th class="ui center aligned two wide">No. Control</th>
+                <th class="ui center aligned">Solicitud</th>
+                <th class="ui center aligned two wide">Tipo</th>
+                <th class="ui center aligned one wide">Paralelo</th>
+                <th class="ui center aligned two wide">Estatus</th>
+                @can('edit_moves')
+                <th class="ui center aligned two wide">
+                  <i class="ui cog icon"></i>
+                </th>
+                @endcan
+              </tr>
+            </thead>
+            <tbody>
+              @forelse ($result as $item)
+              <tr>
+                <td class="ui center aligned">{{ $item->user->username }}</td>
+                <td>{{ $item->type }} DE {{ $item->group->subject->short_name }} ({{ $item->group->full_key }})</td>
+                <td class="ui center aligned">
+                  <small>{{ $item->type }}</small> <i class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i>
+                </td>
+                <td class="ui center aligned">
+                  @if ($item->is_parallel)
+                  <a class="ui blue circular label">P</a>
+                  @else
+                  <i class="ui grey minus icon"></i>
                   @endif
-                </div>
-              </td>
-            </tr>
-            @endforelse
-          </tbody>
-          <tfoot class="full-width">
-            <tr>
-              <th colspan="6">
-                @if (!ends_with(Request::path(), "/all"))
-                  @include('pagination.custom', ['paginator' => $result])
-                @endif
-              </th>
-            </tr>
-          </tfoot>
-        </table>
-        @if (isset($url))
-        @include('components.back', ['route' => $url])
-        @else
-        @include('components.back')
-        @endif
+                </td>
+                <td class="ui center aligned">
+                  @include('shared._move_status', ['status' => $item->status])
+                </td>
+                @can('edit_moves')
+                <td class="ui center aligned">
+                  @role('Estudiante')
+                  @include('moves.student_actions', ['id' => $item->id, 'move' => $item])
+                  @endrole
+                  @hasanyrole(['Coordinador','Jefe','Admin'])
+                  @include('moves.coordinator_actions', ['id' => $item->id])
+                  @endhasanyrole
+                </td>
+                @endcan
+              </tr>
+              @empty
+              <tr>
+                <td colspan="6">
+                  <div class="ui placeholder segment">
+                    <div class="ui icon header">
+                      <i class="inbox icon"></i>
+                      @role('Estudiante')
+                      Aún no has solicitado movimientos
+                      @else
+                      No hay movimientos solicitados
+                      @endrole
+                    </div>
+                    @if (!is_null(Auth::user()->career))
+                    @role('Estudiante')
+                    <div class="inline">
+                      <a href="{{ route('moves.new',['type' => 'up']) }}" class="ui blue icon labeled button">
+                        <i class="ui sort up icon"></i>
+                        Altas
+                      </a>
+                      <a href="{{ route('moves.new',['type' => 'down']) }}" class="ui red icon labeled button">
+                        <i class="ui sort down icon"></i>
+                        Bajas
+                      </a>
+                    </div>
+                    @endrole
+                    @endif
+                  </div>
+                </td>
+              </tr>
+              @endforelse
+            </tbody>
+            <tfoot class="full-width">
+              <tr>
+                <th colspan="6">
+                  @if (!ends_with(Request::path(), "/all"))
+                    @include('pagination.custom', ['paginator' => $result])
+                  @endif
+                </th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+
+        <div class="mobile only sixteen wide column">
+          @forelse ($result as $item)
+          <div class="ui card">
+            <div class="content">
+              <div class="header">{{ $item->group->subject->short_name }} ({{ $item->group->full_key }})</div>
+              <div class="meta">
+                <span class="right floated">
+                  {{ $item->user->username }}
+                </span>
+                <span class="category"><small>{{ $item->type }}</small> <i class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i></span>
+              </div>
+            </div>
+            <div class="extra content">
+              <div class="left floated">
+                @if ($item->is_parallel)
+                  <a class="ui blue label">PARALELO</a>
+                  @endif
+              </div>
+              <div class="right floated">
+                @include('shared._move_status', ['status' => $item->status])
+              </div>
+            </div>
+            @can('edit_moves')
+            @role('Estudiante')
+            @include('moves.student_actions', ['id' => $item->id, 'move' => $item])
+            @endrole
+            @hasanyrole(['Coordinador','Jefe','Admin'])
+            @include('moves.coordinator_actions', ['id' => $item->id])
+            @endhasanyrole
+            @endcan
+          </div>
+
+          @empty
+          <div class="segment">No hay movimientos solicitados</div>
+          @endforelse
+          @if (!ends_with(Request::path(), "/all"))
+            @include('pagination.custom', ['paginator' => $result])
+          @endif
+
+          <div class="ui row sixteen wide column">
+            @if (isset($url))
+            @include('components.back', ['route' => $url])
+            @else
+            @include('components.back')
+            @endif
+          </div>
+        </div>
+        <div class="ui row sixteen wide column">
+          @if (isset($url))
+          @include('components.back', ['route' => $url])
+          @else
+          @include('components.back')
+          @endif
+        </div>
       </article>
     </section>
   </div>
