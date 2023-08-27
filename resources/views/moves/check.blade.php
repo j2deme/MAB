58c549391bd7a787c63a4f3afd07c60dec870bee
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2023-08-27 16:49:24 -0600

    📱 Agrega responsividad a solicitud individual

--- resources/views/moves/check.blade.php
+++ resources/views/moves/check.blade.php
@@ -8,69 +8,106 @@
       <article class="ui attached segment">
         <header>
           <h2 class="ui primary dividing header">
-            Revisar solicitud
+          @if (in_array($move->status, ['0','1']))
+            Atender solicitud
+          @endif
+            Modificar respuesta de solicitud
           </h2>
         </header>
 
+        <div class="tablet only computer only large screen only widescreen only sixteen wide column">
+          <table class="ui celled compact table">
+            <thead>
+              <tr>
+                <th class="ui center aligned">Estudiante</th>
+                <th class="ui center aligned">Carrera</th>
+                <th class="ui center aligned four wide">Materia</th>
+                <th class="ui center aligned two wide">Solicitud</th>
+                <th class="ui center aligned">Estatus</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="ui center aligned">
+                  <strong>{{ $move->user->username }}</strong><br>
+                  {{ $move->user->full_name }}
+                </td>
+                <td class="ui center aligned">{{ $move->user->career->key }}</td>
+                <td class="ui center aligned">{{ $move->group->subject->short_name }}
+                  ({{ $move->group->full_key }})</td>
+                <td class="ui center aligned">
+                  <strong>{{ $move->type }}</strong>&nbsp;
+                  @if($move->type == "ALTA")
+                    <i class="ui arrow up blue icon"></i>
+                  @else
+                    <i class="ui arrow down red icon"></i>
+                  @endif
+                  @if ($move->is_parallel)
+                    <span class="ui blue label">P</span>
+                  @endif
+                </td>
+                <td class="ui center aligned">
+                  @include('shared._move_status', ['status' => $move->status])
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <table class="ui celled compact table">
+            <thead>
+              <tr>
+                <th class="ui center aligned five wide">Motivo</th>
+                <th class="ui center aligned">Extras</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="ui center aligned">{{ $move->justification['main'] }}</td>
+                <td class="ui center aligned">{{ $move->justification['extra'] }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="mobile only sixteen wide column">
+          <div class="ui horizontal card">
+            <div class="content">
+              <div class="header">{{ $move->group->subject->short_name }} ({{ $move->group->full_key }})</div>
+              <div class="meta">
+                <span class="right floated">
+                  {{ $move->user->username }} <small>({{ $move->user->career->key }})</small>
+                </span>
+                <span class="category"><small>{{ $move->type }}</small> <i class="ui {{ $move->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i></span>
+              </div>
+              <div class="description">
+                <h4>{{ $move->justification['main'] }}</h4>
+                {{ $move->justification['extra'] }}
+              </div>
+            </div>
+            <div class="extra content">
+              <div class="left floated">
+                @if ($move->is_parallel)
+                  <a class="ui blue label">PARALELO</a>
+                @endif
+              </div>
+              <div class="right floated">
+                @include('shared._move_status', ['status' => $move->status])
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="ui hidden divider"></div>
         <form action="{{ route('moves.update',['move'=>$move]) }}" class="ui form" method="POST">
           @csrf
           @method('PUT')
           <input type="hidden" name="url" id="url" value="{{ url()->previous() }}">
           <input type="hidden" id="move_id" name="move_id" value="{{ $move->id }}">
-          <div class="field">
-            <table class="ui celled compact table">
-              <thead>
-                <tr>
-                  <th class="ui center aligned one wide">No. Control</th>
-                  <th class="ui center aligned">Nombre</th>
-                  <th class="ui center aligned two wide">Carrera</th>
-                  <th class="ui center aligned two wide">
-                    <i class="ui eye icon"></i>
-                  </th>
-                  <th class="ui center aligned four wide">Solicitud</th>
-                  <th class="ui center aligned one wide">Paralelo</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td class="ui center aligned">{{ $move->user->username }}</td>
-                  <td class="ui center aligned">{{ $move->user->full_name }}</td>
-                  <td class="ui center aligned">{{ $move->user->career->key }}</td>
-                  <td class="ui center aligned">
-                    <a href="http://167.114.218.98/SEGRET/caso/{{ $move->user->username }}" target="_blank">Avance</a>
-                  </td>
-                  <td class="ui center aligned">{{ $move->type }} DE {{ $move->group->subject->short_name }}
-                    ({{ $move->group->full_key }})</td>
-                  <td class="ui center aligned">
-                    @if ($move->type == 'BAJA')
-                    <i class="ui large grey minus icon"></i>
-                    @else
-                    <i class="ui large {{ $move->is_parallel ? 'green check' : 'red times' }} icon"></i>
-                    @endif
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <table class="ui celled compact table">
-              <thead>
-                <tr>
-                  <th class="ui center aligned">Motivo</th>
-                  <th class="ui center aligned">Extras</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td class="ui center aligned">{{ $move->justification['main'] }}</td>
-                  <td class="ui center aligned">{{ $move->justification['extra'] }}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div class="field">
-            <label for="answer">Respuesta rápida</label>
-            <select id="answer" name="answer" class="ui search selection dropdown">
-              <option value="">Elija una respuesta predefinida</option>
-              @foreach ($answers as $item)
+
+          <div class="ui two fields">
+            <div class="field">
+              <label for="answer">Respuesta rápida</label>
+              <select id="answer" name="answer" class="ui search selection dropdown">
+                <option value="">Elija una respuesta predefinida</option>
+                @foreach ($answers as $item)
                 <option value="{{ $item }}"
                 {{ (isset($move->answer['main']) and $move->answer['main'] == $item) ? 'selected' : '' }}
                 >{{ $item }}</option>
