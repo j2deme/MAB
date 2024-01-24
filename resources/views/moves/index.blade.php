5e30524a1777748a25f102dd3c642b8566f3c4fb
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-01-24 15:46:35 -0600

    🎨 Ajusta visualización de indicador de paralelo

--- resources/views/moves/index.blade.php
+++ resources/views/moves/index.blade.php
@@ -20,16 +20,16 @@ Path: {{ Request::path() }}
         </header>
         <span class="ui sub header">
           @if (isset($ups))
-            {{ $ups }} <i class="ui blue up arrow icon"></i> 
+          {{ $ups }} <i class="ui blue up arrow icon"></i>
           @endif
           &emsp;
           @if (isset($downs))
-            {{ $downs }} <i class="ui red down arrow icon"></i> 
+          {{ $downs }} <i class="ui red down arrow icon"></i>
           @endif
         </span>
 
         <div class="ui right floated small buttons">
-        @role('Estudiante')
+          @role('Estudiante')
           <a href="{{ route('moves.new',['type' => 'up']) }}" class="ui blue icon labeled button">
             <i class="ui sort up icon"></i>
             Altas
@@ -38,24 +38,24 @@ Path: {{ Request::path() }}
             <i class="ui sort down icon"></i>
             Bajas
           </a>
-        @endrole
-        @hasanyrole(['Jefe','Admin','Coordinador'])
-        @if(isset($search) and $search == true)
-        <form action="{{ route('moves.listAttended') }}" method="GET" class="ui horizontal form">
-          <div class="ui icon input">
-            <input type="text" name="search" placeholder="Buscar..." autocomplete="off">
-            <i class="search icon"></i>
-          </div>
-        </form>
-        @endif
-        @include('components.list-all')
-        @if (isset($user) and !$user->is_enrolled)
+          @endrole
+          @hasanyrole(['Jefe','Admin','Coordinador'])
+          @if(isset($search) and $search == true)
+          <form action="{{ route('moves.listAttended') }}" method="GET" class="ui horizontal form">
+            <div class="ui icon input">
+              <input type="text" name="search" placeholder="Buscar..." autocomplete="off">
+              <i class="search icon"></i>
+            </div>
+          </form>
+          @endif
+          @include('components.list-all')
+          @if (isset($user) and !$user->is_enrolled)
           <a href="{{ route('users.singleActivate', ['key'=> $user->username]) }}" class="ui green icon labeled button">
             <i class="ui sort down icon"></i>
             Inscribir
           </a>
-        @endif
-        @endhasanyrole
+          @endif
+          @endhasanyrole
         </div>
 
         <div class="tablet only computer only large screen only widescreen only sixteen wide column">
@@ -66,29 +66,27 @@ Path: {{ Request::path() }}
                   @role('Estudiante')
                   #
                   @else
-                    @if(isset($ups))
-                    <a href="?sort=student">No. Control</a>
-                    @else
-                    No. Control
-                    @endif
+                  @if(isset($ups))
+                  <a href="?sort=student">No. Control</a>
+                  @else
+                  No. Control
+                  @endif
                   @endrole
                 </th>
                 <th class="ui center aligned">Solicitud</th>
                 <th class="ui center aligned one wide">Semestre</th>
                 @if (isset($ups))
-                  <th class="ui center aligned one wide"><a href="?sort=group">Grupo</a></th>
-                  <th class="ui center aligned two wide"><a href="?sort=type">Tipo</a></th>
-                  @hasanyrole(['Admin','Jefe'])
-                  <th class="ui center aligned one wide">Paralelo</th>
-                  @endhasanyrole
-                  <th class="ui center aligned two wide"><a href="?sort=status">Estatus</a></th>
+                <th class="ui center aligned one wide"><a href="?sort=group">Grupo</a></th>
+                <th class="ui center aligned two wide"><a href="?sort=type">Tipo</a></th>
+                @hasanyrole(['Admin','Jefe'])
+                <th class="ui center aligned one wide">Paralelo</th>
+                @endhasanyrole
+                <th class="ui center aligned two wide"><a href="?sort=status">Estatus</a></th>
                 @else
-                  <th class="ui center aligned one wide">Grupo</th>
-                  <th class="ui center aligned two wide">Tipo</th>
-                  @hasanyrole(['Admin','Jefe'])
-                  <th class="ui center aligned one wide">Paralelo</th>
-                  @endhasanyrole
-                  <th class="ui center aligned two wide">Estatus</th>
+                <th class="ui center aligned one wide">Grupo</th>
+                <th class="ui center aligned two wide">Tipo</th>
+                <th class="ui center aligned one wide">Paralelo</th>
+                <th class="ui center aligned two wide">Estatus</th>
                 @endif
                 @can('edit_moves')
                 <th class="ui center aligned two wide">
@@ -101,19 +99,19 @@ Path: {{ Request::path() }}
               @forelse ($result as $key => $item)
               <tr>
                 <td class="ui center aligned">
-                @role('Estudiante')
-                {{ $key + 1 }}
-                @else
-                {{ $item->user->username }}
-                @endrole
+                  @role('Estudiante')
+                  {{ $key + 1 }}
+                  @else
+                  {{ $item->user->username }}
+                  @endrole
                 </td>
                 <td>{{ $item->group->subject->short_name }} ({{ $item->group->subject->key }})</td>
                 <td class="ui center aligned">{{ $item->group->subject->semester }}</td>
                 <td class="ui center aligned">{{ $item->group->name }}</td>
                 <td class="ui center aligned">
-                  <small>{{ $item->type }}</small> <i class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i>
+                  <small>{{ $item->type }}</small> <i
+                    class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i>
                 </td>
-                @hasanyrole(['Admin','Jefe'])
                 <td class="ui center aligned">
                   @if ($item->is_parallel)
                   <a class="ui blue label">P</a>
@@ -121,7 +119,6 @@ Path: {{ Request::path() }}
                   <i class="ui grey minus icon"></i>
                   @endif
                 </td>
-                @endhasanyrole
                 <td class="ui center aligned">
                   @include('shared._move_status', ['status' => $item->status])
                 </td>
@@ -157,7 +154,7 @@ Path: {{ Request::path() }}
               <tr>
                 <th colspan="8">
                   @if (!ends_with(Request::path(), "/all"))
-                    @include('pagination.custom', ['paginator' => $result])
+                  @include('pagination.custom', ['paginator' => $result])
                   @endif
                 </th>
               </tr>
@@ -174,13 +171,14 @@ Path: {{ Request::path() }}
                 <span class="right floated">
                   {{ $item->user->username }}
                 </span>
-                <span class="category"><small>{{ $item->type }}</small> <i class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i></span>
+                <span class="category"><small>{{ $item->type }}</small> <i
+                    class="ui {{ $item->type == 'ALTA' ? 'blue arrow up' : 'red arrow down' }} icon"></i></span>
               </div>
             </div>
             <div class="extra content">
               <div class="left floated">
                 @if ($item->is_parallel)
-                  <a class="ui blue label">PARALELO</a>
+                <a class="ui blue label">PARALELO</a>
                 @endif
               </div>
               <div class="right floated">
@@ -201,14 +199,14 @@ Path: {{ Request::path() }}
           <div class="segment">No hay movimientos solicitados</div>
           @endforelse
           @if (!ends_with(Request::path(), "/all"))
-            @include('pagination.custom', ['paginator' => $result])
+          @include('pagination.custom', ['paginator' => $result])
           @endif
         </div>
         <div class="ui row sixteen wide column">
           @if (isset($url))
-            @include('components.back', ['route' => $url])
+          @include('components.back', ['route' => $url])
           @else
-            @include('components.back')
+          @include('components.back')
           @endif
         </div>
       </article>
